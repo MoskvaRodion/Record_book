fff43f8a8b22d5ec9af8ec07eee9f472a2d6489a
Никита <59824002+Sok-ai@users.noreply.github.com>
2024-05-17 20:43:31 +0300

    input&Style
    Co-Authored-By: MoskvaRodion <74708306+MoskvaRodion@users.noreply.github.com>

--- import/index.php
+++ import/index.php
@@ -9,21 +9,12 @@ if($USER->isAuthorized() && in_array(1, $USER->GetUserGroupArray())){?>
     <div class="container">
         <div class="">
             <form class="import-form" action="/import/import.php" method="post" enctype="multipart/form-data">
-                <input type="file" name="file" value="" accept=".xlsx"><br><br>
+                <input type="file" name="file" value="" accept=".xlsx" multiple><br><br>
                 <button class="btn" type="submit">Выполнить</button>
             </form>
         </div>
     </div>
 </section>
-<style>
-    .import-form{
-        padding-bottom:100px;
-    }
-    .import-form select{
-        padding:10px;
-        max-width:100%;
-    }
-</style>
 <script>
     function IsJsonString(str) {
         try {
