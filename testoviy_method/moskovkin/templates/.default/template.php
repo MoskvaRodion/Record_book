8cf1001d8d532078df68906a2ebb072735bb5812
Rodion <moskovkinrodion2004@yandex.ru>
2024-05-17 19:59:25 +0300

    update desing page
    Co-Authored-By: Никита <59824002+Sok-ai@users.noreply.github.com>

--- testoviy_method/moskovkin/templates/.default/template.php
+++ testoviy_method/moskovkin/templates/.default/template.php
@@ -31,55 +31,54 @@ function colotEstimation($estimation){
   }
 }
 ?>
-<head>
 
-</head>
-<main>
-        <div class="main_content">
-          <div class="main_content-left">
+<div class="main_content">
+      <div class="main_content-left">
             <div class="main_content-leftInternal">
-              <h1 class="main_content-leftInternal__title"><?=$student?></h1>
-              <p class="main_content-leftInternal__group"><?=$group?></p>
-              <div class="main_content-leftInternal__profession">
-                <span>Специальность</span>
-                <p class="main_content-leftInternal__profession-text">Информационные системы и програмирование</div>
-              </div>
+                <p class="main_content-leftInternal__group"><?= $group ?></p>
+                <h1 class="main_content-leftInternal__title"><?= $student?></h1>
+                <p class="main_content-leftInternal__profession-text">Информационные системы и програмирование</p>
             </div>
+        </div>
             <div class="main_content-right">
                 <div class="main_content-rightInternal">
-                  <?
-                
-if (!empty($arResult)){
-    foreach($arResult as $arItem){?>
-      <div class="semestr"><?=$arItem['NAME'];?></div>
-        <table class="table-estimation" width="450">
-            <thead>
-              <tr>
-                <th scope="col">№</th>
-                <th scope="col">Дисциплина</th>
-                <th scope="col">Тип оценки</th>
-                <th scope="col">Оценка</th>
-              </tr>
-            </thead>
-            <tbody>
-            <?foreach($arItem['~PROPERTY_COMPLEX_VALUE'] as $arSubGrade){?>
-                <tr>
-                  <td></th>
-                  <td><?=$arSubGrade['subject'];?></td>
-                  <td><?=$arSubGrade['type'];?></td>
-                  <td><span class="<?= colotEstimation($arSubGrade['estimation']); ?>"><?=$arSubGrade['estimation'];?></span></td>
-                </tr>
-            <?}?>
-            </tbody>
-        </table>
-    <?}
-}
-else {
-    echo "Ничего не найдено";
-}
-?>
-              </div>
-          </div>
+                    <select name="semester" id="semester">
+                        <option selected value="semester1">1 семестр 2022-2023 у.г.</option>
+                        <option value="semester2">2 семестр 2023-2024 у.г.</option>
+                    </select>
+                    <?
+        
+        if (!empty($arResult)){
+            foreach($arResult as $arItem){?>
+              <div class="semestr"><?=$arItem['NAME'];?></div>
+                <table class="table-estimation">
+                  <thead>
+                    <tr>
+                      <th scope="col">№</th>
+                      <th scope="col">Дисциплина</th>
+                      <th scope="col">Тип оценки</th>
+                      <th scope="col">Оценка</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?foreach($arItem['~PROPERTY_COMPLEX_VALUE'] as $arSubGrade){?>
+                        <tr>
+                          <td></th>
+                          <td><?=$arSubGrade['subject'];?></td>
+                          <td><?=$arSubGrade['type'];?></td>
+                          <td><span class="<?= colotEstimation($arSubGrade['estimation']); ?>"><?=$arSubGrade['estimation'];?></span></td>
+                        </tr>
+                    <?}?>
+                    </tbody>
+                </table>
+            <?}
+        }
+        else {
+            echo "Ничего не найдено";
+        }
+        ?>
+        
+                </div>
+            </div>
         </div>
-      </div>
-    </main>
+    </div>
--- zachetnaya-knijka.php
+++ zachetnaya-knijka.php
@@ -3,6 +3,7 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Зачетная книжка");
 ?>
 <?
+
 global $USER;
 if($USER->IsAuthorized()){
 
