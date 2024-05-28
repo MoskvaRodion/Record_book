2c4f2aae48e3a00fddbb24895e67bfa2e5bba761
Rodion <moskovkinrodion2004@yandex.ru>
2024-05-28 20:42:19 +0300

    Добавлены шаблоны заявлений

--- print-shablon/index.php
+++ print-shablon/index.php
@@ -40,12 +40,79 @@ if($USER->isAuthorized()) {
             </form>
             <!-- ------- -->
             <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data"> 
-                <label for="reason">Причина замены пропуска</label>
+                <label class="label-10" for="reason">Причина замены пропуска</label>
                 <select name="reason" id="reason" class="form-control">
                     <option value="был утерян" selected="selected">Потерял</option>
                     <option value="не работает">Не работает</option>
                 </select>
                 <button  class="submit btn btn_main" type="submit" name="button" value="pass">Сформировать</button>
+            </form>
+            <!-- ---------- -->
+            <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data">
+                <button  class="submit btn btn_main" type="submit" name="button" value="duplicate-IDcard">Сформировать</button>
+            </form>
+            <!-- ---------- -->
+            <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data">
+                <button  class="submit btn btn_main" type="submit" name="button" value="duplicate-record-book">Сформировать</button>
+            </form>
+            <!-- ---------- -->
+            <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data">
+                <label class="label-10" for="FROM">Начало отпуска</label>
+                <input  class="form-control" name="FROM" type="date">
+                <label class="label-10" for="TO">Окончание отпуска</label>
+                <input class="form-control" name="TO" type="date">
+                <span>Не забудьте приложить копию повестки военного комиссариата</span>
+                <button  class="submit btn btn_main" type="submit" name="button" value="army">Сформировать</button>
+            </form>
+            <!-- ---------- -->
+            <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data">
+                <label class="label-10" for="FROM">Начало отпуска</label>
+                <input  class="form-control" name="FROM" type="date">
+                <label class="label-10" for="TO">Окончание отпуска</label>
+                <input  class="form-control" name="TO" type="date">
+                <span>Не забудьте приложить копию медицинской справки</span>
+                <button  class="submit btn btn_main" type="submit" name="button" value="health">Сформировать</button>
+            </form>
+             <!-- ---------- -->
+            <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data">
+                <label class="label-10">Выбирите что изменяете</label>
+                <select  class="form-control">
+                    <option value="Фамилия">Фамилия</option>
+                    <option value="Имя">Имя</option>
+                    <option value="Отчество">Отчество</option>
+                </select>
+                <input  class="form-control" type="text" name="AFTER" placeholder="ФИО полностью после изменения">
+                <label class="label-10">Выбирите документ удостовряющий смену ФИО</label>
+                <select name="DOCUMENT" id=""  class="form-control">
+                    <option value="Свидетельство о браке">Свидетельство о браке</option>
+                    <option value="Свидетельство о смене имени">Свидетельство о смене имени</option>
+                </select>
+                <span>Не забудьте приложить копию этого документа</span>
+                <button  class="submit btn btn_main" type="submit" name="button" value="change">Сформировать</button>
+            </form>
+            <!-- ------------------- -->
+            <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data">
+                <div>
+                    <input type="radio" name="choice" checked>
+                    <label for="choice" if>Специальность</label>
+                </div>
+                <div>
+                    <input type="radio" name="choice">
+                    <label for="choice">Форма обучения</label>
+                </div>
+                <input type="text" name="ANOTHER_SPECIALTY" id="" placeholder="Специальность" >
+                <select name="FORM_OF_EDUCATION" id="">
+                    <option value="Очная">Очная</option>
+                    <option value="Заочная">Заочная</option>
+                </select>
+                <span>Не забудте приложить копию зачетной книжки</span>
+                <button  class="submit btn btn_main" type="submit" name="button" value="translation-specialty">Сформировать</button>
+            </form>
+            <!-- ------------------- -->
+            <form action="/print-shablon/output-data.php" class="form"  method="POST" enctype="multipart/form-data">
+                <label class="label-10" for="ORG">Наименование принимающей организации</label>
+                <input type="text" name="ORG" id="">
+                <button  class="submit btn btn_main" type="submit" name="button" value="transfer-to-another-college">Сформировать</button>
             </form>
         </div>
     </section>
--- print-shablon/output-data.php
+++ print-shablon/output-data.php
@@ -53,6 +53,130 @@ else if ($_POST["button"]=='pass'){
     header("Content-Disposition: attachment; filename=Замена_пропуска_".$LAST_NAME.".docx");
     echo file_get_contents("files/v4.docx");
 }
+elseif ($_POST["button"]=='duplicate-IDcard'){
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('files/duplicate-student-ID-card.docx');
+    $templateProcessor->setValue('GROUP', $arParam["UF_GROUP"]);
+    $templateProcessor->setValue('LAST_NAME', $LAST_NAME);
+    $templateProcessor->setValue('NAME', $NAME);
+    $templateProcessor->setValue('SECOND_NAME', $SECOND_NAME);
+
+    $templateProcessor->setValue('NAME_STUDENT', $short_name);
+
+    $templateProcessor->setValue('DATA', date('d.m.Y'));
+    $templateProcessor->saveAs("files/v4.docx");
+
+    header("Content-Disposition: attachment; filename=Дубликат_".$LAST_NAME.".docx");
+    echo file_get_contents("files/v4.docx");
+}
+elseif ($_POST["button"]=='duplicate-record-book'){
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('files/duplicate-record-book.docx');
+    $templateProcessor->setValue('GROUP', $arParam["UF_GROUP"]);
+    $templateProcessor->setValue('LAST_NAME', $LAST_NAME);
+    $templateProcessor->setValue('NAME', $NAME);
+    $templateProcessor->setValue('SECOND_NAME', $SECOND_NAME);
+
+    $templateProcessor->setValue('NAME_STUDENT', $short_name);
+
+    $templateProcessor->setValue('DATA', date('d.m.Y'));
+    $templateProcessor->saveAs("files/v4.docx");
+
+    header("Content-Disposition: attachment; filename=Дубликат_".$LAST_NAME.".docx");
+    echo file_get_contents("files/v4.docx");
+}
+elseif ($_POST["button"]=='army'){
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('files/academic-leave-army.docx');
+    $templateProcessor->setValue('GROUP', $arParam["UF_GROUP"]);
+    $templateProcessor->setValue('LAST_NAME', $LAST_NAME);
+    $templateProcessor->setValue('NAME', $NAME);
+    $templateProcessor->setValue('SECOND_NAME', $SECOND_NAME);
+
+    $templateProcessor->setValue('FROM', $_POST["FROM"]);
+    $templateProcessor->setValue('TO', $_POST["TO"]);
+
+    $templateProcessor->setValue('NAME_STUDENT', $short_name);
+
+    $templateProcessor->setValue('DATA', date('d.m.Y'));
+    $templateProcessor->saveAs("files/v4.docx");
+
+    header("Content-Disposition: attachment; filename=Академический_отпуск".$LAST_NAME.".docx");
+    echo file_get_contents("files/v4.docx");
+}
+elseif ($_POST["button"]=='health'){
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('files/academic-leave-health.docx');
+    $templateProcessor->setValue('GROUP', $arParam["UF_GROUP"]);
+    $templateProcessor->setValue('LAST_NAME', $LAST_NAME);
+    $templateProcessor->setValue('NAME', $NAME);
+    $templateProcessor->setValue('SECOND_NAME', $SECOND_NAME);
+
+    $templateProcessor->setValue('FROM', $_POST["FROM"]);
+    $templateProcessor->setValue('TO', $_POST["TO"]);
+
+    $templateProcessor->setValue('NAME_STUDENT', $short_name);
+
+    $templateProcessor->setValue('DATA', date('d.m.Y'));
+    $templateProcessor->saveAs("files/v4.docx");
+
+    header("Content-Disposition: attachment; filename=Академический_отпуск".$LAST_NAME.".docx");
+    echo file_get_contents("files/v4.docx");
+}
+elseif ($_POST["button"]=='change'){
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('files/name-change.docx');
+    $templateProcessor->setValue('GROUP', $arParam["UF_GROUP"]);
+    $templateProcessor->setValue('LAST_NAME', $LAST_NAME);
+    $templateProcessor->setValue('NAME', $NAME);
+    $templateProcessor->setValue('SECOND_NAME', $SECOND_NAME);
+
+    $templateProcessor->setValue('BEFORE', $LAST_NAME . $NAME . $SECOND_NAME);
+    $templateProcessor->setValue('AFTER', $_POST["AFTER"]);
+    $templateProcessor->setValue('AFTER', $_POST["AFTER"]);
+
+    $templateProcessor->setValue('DOCUMENT', $_POST["DOCUMENT"]);
+
+    $templateProcessor->setValue('DATA', date('d.m.Y'));
+    $templateProcessor->saveAs("files/v4.docx");
+
+    header("Content-Disposition: attachment; filename=Смена_ФИО".$LAST_NAME.".docx");
+    echo file_get_contents("files/v4.docx");
+}
+elseif ($_POST["button"]=='translation-specialty'){
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('files/translation-specialty.docx');
+    $templateProcessor->setValue('GROUP', $arParam["UF_GROUP"]);
+    $templateProcessor->setValue('LAST_NAME', $LAST_NAME);
+    $templateProcessor->setValue('NAME', $NAME);
+    $templateProcessor->setValue('SECOND_NAME', $SECOND_NAME);
+
+    $templateProcessor->setValue('FORM_OF_EDUCATION', $_POST["FORM_OF_EDUCATION"]);
+    $templateProcessor->setValue('ANOTHER_SPECIALTY', $_POST["ANOTHER_SPECIALTY"]);
+
+    $templateProcessor->setValue('NAME_STUDENT', $short_name);
+    $templateProcessor->setValue('DATA', date('d.m.Y'));
+    $templateProcessor->saveAs("files/v4.docx");
+
+    header("Content-Disposition: attachment; filename=Смена_специальности ".$LAST_NAME.".docx");
+    echo file_get_contents("files/v4.docx");
+}
+else if ($_POST["button"]=='transfer-to-another-college'){
+    $parent_name = explode(" ", $_POST["parent_name"]);
+    $short_parent_name = $parent_name[0] ." ". mb_substr($parent_name[1], 0, 1).".".mb_substr($parent_name[2], 0, 1).".";
+
+    $templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor('files/transfer-to-another-college.docx');
+
+    $templateProcessor->setValue('GROUP', $arParam["UF_GROUP"]);
+    $templateProcessor->setValue('LAST_NAME', $LAST_NAME);
+    $templateProcessor->setValue('NAME', $NAME);
+    $templateProcessor->setValue('SECOND_NAME', $SECOND_NAME);
+    $templateProcessor->setValue('ORG', $_POST["ORG"]);
+
+    $templateProcessor->setValue('NAME_STUDENT', $short_name);
+    $templateProcessor->setValue('PARENT', $_POST["parent"]);
+    $templateProcessor->setValue('NAME_PARENT', $short_parent_name);
+    $templateProcessor->setValue('DATA', date('d.m.Y'));
+
+    $templateProcessor->saveAs("files/v4.docx");
+
+    header("Content-Disposition: attachment; filename=Отчисление_".$LAST_NAME.".docx");
+    echo file_get_contents("files/v4.docx");
+}
 
 
 ?>
